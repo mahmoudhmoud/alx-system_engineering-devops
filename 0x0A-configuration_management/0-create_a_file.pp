e6e09a704cb58d3c043a56f3b4b0f039b91bb152
mahmoudhmoud <jadmclarence@gamil.com>
2024-04-19 13:02:06 -0700

    aete

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 file {
-'school':
+'/tmp/school':
+  ensure  => 'file',
   content => 'I love Puppet',
+  owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/school',
 
 }
-# sawab milaf fih
+# make a tmp file
