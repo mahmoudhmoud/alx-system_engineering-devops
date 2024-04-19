b61b7a3264721aa0de517006ed51e0218fa86a80
mahmoudhmoud <jadmclarence@gamil.com>
2024-04-19 11:48:52 -0700

    aoiuoetwe

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+file {
+'school':
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+
+}
+# sawab milaf fih
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+is for 0x0A conffiguration  management
